fdf07c742b8e072016fb081de88dbec6cf948c4c
yaoyue <yaoyue@fjwr.xyz>
2025-09-22 11:11:34 +0800

    修复无法正确拉取服务端二进制文件的问题

--- server/get_latest_server.sh
+++ server/get_latest_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-LATEST_VER=$(curl -s https://api.github.com/repos/perfect-panel/ppanel/releases/latest | jq -r '.tag_name')
-SERVER_URL="https://github.com/perfect-panel/ppanel/releases/download/${LATEST_VER}/ppanel-server-${LATEST_VER}-linux-amd64.tar.gz"
+LATEST_VER=$(curl -s https://api.github.com/repos/perfect-panel/server/releases/latest | jq -r '.tag_name')
+SERVER_URL="https://github.com/perfect-panel/server/releases/download/${LATEST_VER}/ppanel-server-linux-amd64.tar.gz"
 mkdir -p /opt/ppanel
 wget -O /opt/ppanel-server.tar.gz $SERVER_URL
 tar -xvf /opt/ppanel-server.tar.gz -C /opt/ppanel/
