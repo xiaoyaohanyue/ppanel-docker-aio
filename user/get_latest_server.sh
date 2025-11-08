ab98376158ddc9c23286c7783da088994233dd5e
yaoyue <yaoyue@fjwr.xyz>
2025-11-08 12:57:33 +0800

    修复前端拉取错误版本问题

--- user/get_latest_server.sh
+++ user/get_latest_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LATEST_VER=$(curl -s https://api.github.com/repos/perfect-panel/ppanel-web/releases | jq -r '[.[] | select(.prerelease == true)][0].tag_name')
+LATEST_VER=$(curl -s https://api.github.com/repos/perfect-panel/ppanel-web/releases/latest | jq -r '.tag_name')
 SERVER_URL="https://github.com/perfect-panel/ppanel-web/releases/download/${LATEST_VER}/ppanel-user-web.tar.gz"
 mkdir -p /opt/ppanel
 wget -O /opt/ppanel-user-web.tar.gz $SERVER_URL
